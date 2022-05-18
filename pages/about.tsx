ad760ed8e290c5fa95935515541c9a73f2002646
wedburst <jhonrawed@gmail.com>
2022-05-18 15:06:50 -0500

    limpiando about y indext page

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,13 +1,10 @@
-import { useContext } from 'react';
 import { Layout } from '../components/layouts';
-import { UIContext } from '../context';
 
 const AboutPage = () => {
-  const {themeColor} = useContext(UIContext)
 
   return (
     <Layout>
-        <h1>About Page {themeColor}</h1>
+        <h1>About Page</h1>
     </Layout>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,11 @@
 import type { NextPage } from 'next'
-import { useContext } from 'react';
 import { Layout } from '../components/layouts';
-import { UIContext } from '../context';
 
 const Home: NextPage = () => {
-  const {themeColor} = useContext(UIContext)
 
   return (
     <Layout>
-      <h1>Cookie Master {themeColor}</h1>
+      <h1>Cookie Master</h1>
     </Layout>
   )
 }
